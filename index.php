81251f813bf8f1236ddbcae5d73ab7ffb56ec294
yanis <yanis.legrand.1@gmail.com>
2021-05-03 15:25:07 +0200

    03/05/2021

--- index.php
+++ index.php
@@ -4,11 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
+<div class="container">
+  <div class="row">
+    <div class="alert alert-primary" role="alert">
+      Vous etes connecté !
+    </div>
     <?php
-    $bdd= mysqli_connect('mysql-yaperson.alwaysdata.net', 'yaperson', 'voltor123', 'yaperson_bibliotheque');
+    session_start();
+    include_once('conf.php');
+    $bdd = mysqli_connect(SERVEUR,USER,MDP,DB);
     $resultat = mysqli_query($bdd,'SELECT libelle FROM type_livre ');
     echo mysqli_num_rows($resultat);
     echo '</br>';
@@ -19,13 +27,12 @@
     function  ConsulterBD (){
         //--- Début de table en HTML
         echo "<center>" ;
-        echo "<table border>" ;
+        echo "<table class='table'>" ;
         echo "<caption> <h2> Clients </h2> </caption>" ;
         echo "<tr> <th> Nom </th> <th> Prénom </th> <th> Age </th> <th> Sexe </th> <th> Metier </th> <th> Departement </th> </tr>" ;
       
         //--- Connection au SGBDR 
         $DataBase = mysqli_connect ( "localhost" , "root" , "" ) ;
-      
         //--- Ouverture de la base de données
         mysqli_select_db ( $DataBase, "bd04" ) ;
       
@@ -70,5 +77,7 @@
 
       ConsulterBD();
     ?>
+    </div>
+</div>
 </body>
 </html>
